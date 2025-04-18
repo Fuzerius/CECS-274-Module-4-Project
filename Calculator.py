555d2dc9c37fffe5654596e0a5036431745b88b8
Fuzerius <139658690+Fuzerius@users.noreply.github.com>
2025-04-18 12:17:03 -0700

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,9 @@
 import re
 import time
+import operator
 import SLLStack
 import ChainedHashTable
+import BinaryTree
 
 
 class Calculator:
@@ -24,7 +26,6 @@ class Calculator:
                     return False
                 stack.pop()
         return True if stack.size() == 0 else False
-
 
     def store_var(self, variable, value, replace=False):
         """
@@ -63,9 +64,10 @@ class Calculator:
 
     def print_stored_vars(self):
         """
-        Displays all math variables and their corresponding values stored
-        on this instance of Calculator.
+        displays all math variables and their corresponding values stored
+        on this instance of Calculator
         """
+        # FIXME: Replace with your implementation from Module 4
         start_time = time.time()
 
         # Get all keys from the hash table
@@ -78,32 +80,127 @@ class Calculator:
         for k in keys:
             print(f"{k} = {self.dict.find(k)}")
         print(f"Displayed all stored keys with corresponding values.\nAction completed in {elapsed_time} seconds.")
-      
 
-    def print_expression(self, expression):
-        """
-        displays the given expression with variables replaced with their values
-        stored on this instance of Calculator
-        """
-        variables = [x for x in re.split('\W+', expression) if x.isalnum()]
-        everything_else = re.split('\w+', expression)
-        start_time = time.time()
-        for i in range(len(variables)):
-            var = variables[i]
-            val = self.dict.find(var)
-            if val is not None:
-                variables[i] = val
-        i = 0
-        j = 0
-        while i < len(variables) and j < len(everything_else):
-            print(everything_else[j] + str(variables[i]), end="")
-            i += 1
-            j += 1
-        while j < len(everything_else):
-            print(everything_else[j], end="")
-            j += 1
-        while i < len(variables):
-            print(str(variables[i]), end="")
-            i += 1
-        elapsed_time = time.time() - start_time
-        print(f"\nPrinted expression with stored variables.\nAction completed in {elapsed_time} seconds.")
+      
+    def _build_parse_tree(self, expr: str) -> str:
+      """
+      helper method to evaluate(expression); builds the parse tree for the given expression
+      """
+      if not self.balanced_parens(expr):
+          raise ValueError("Expression contains unmatched parenthesis.")
+      t = BinaryTree.BinaryTree()
+      t.r = BinaryTree.BinaryTree.Node()
+
+      current = t.r
+      variables, tokens = self._make_tokens(expr)
+      for token in tokens:
+          if token == '(':
+            # FIXME: Replace this with the correct lines of code.
+            current.insert_left(BinaryTree.BinaryTree.Node())
+            current = current.left
+          elif token in ['+', '-', '*', '/']:
+            # FIXME: Replace this with the correct lines of code.
+            current.set_val(token)
+            current.set_key(token)
+            current.insert_right(BinaryTree.BinaryTree.Node())
+            current = current.right
+          elif token == ')':
+            # FIXME: Replace this with the correct lines of code.
+            current = current.parent
+          elif token in variables:
+            # FIXME: Replace this with the correct lines of code.
+            current.set_key(token)
+            current.set_val(self.dict.find(token))
+            current = current.parent
+          else:
+              raise ValueError(f"{token} is an invalid token in the expression")
+
+      return t
+
+    def _evaluate(self, u: BinaryTree.BinaryTree.Node):
+      """
+      helper method to evaluate(expr); evaluates the parse tree rooted at node u
+      :param u: BinaryTree.Node type
+      """
+      oper_dict = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+
+      if u.left is not None and u.right is not None:  # this is an operator
+          operation = oper_dict[u.v]
+          return operation(self._evaluate(u.left), self._evaluate(u.right))
+      elif u.left is None and u.right is None:  # this must be a variable
+          if u.k is None:
+              raise ValueError(f"Missing operand after {u.parent.k}.")
+          elif u.v is not None:
+            
+              return u.v  #FIXME: Replace None with the correct value
+          
+          else:
+              raise ValueError(f"Missing value for variable '{u.k}'")
+      else:
+          raise ValueError(f"Missing an operand and/or operator before {u.left.v}.")
+
+
+    """THE METHODS BELOW ARE FULLY IMPLEMENTED AND DO NOT NEED TO BE MODIFIED"""
+    def expr_with_values(self, expression):
+      """
+      creates a copy of the given expression with variables replaced by stored values
+      :param expression: str type;
+      :return: tuple type; a tuple with two elements; the first is the expression as a string
+               with replaced values for stored variables; the second element is a list of
+               variables in the expression that have no stored value.
+      """
+      remaining_vars = []
+      variables, tokens = self._make_tokens(expression)
+      for i in range(len(tokens)):
+          token = tokens[i]
+          if token in variables:
+              val = self.dict.find(token)
+              if val is None:
+                  remaining_vars.append(token)
+              else:
+                  tokens[i] = val
+      new_expression = ''.join([str(t) for t in tokens])
+      return (new_expression, remaining_vars)
+  
+    def _make_tokens(self, expr: str) -> list:
+      """
+      helper method; creates a list of tokens out of the given expression.
+      A token is defined to be a left/right parenthesis, a variable, or an operator.
+      :param expr: the expression
+      :return: tuple type; a tuple with two elements.  The first element is a list
+               of variables existing in the expression; the second element is the list
+               of all tokens, in the order that they appear in the expression.
+      """
+      temp_vars = [x for x in re.split('\W+', expr) if x.isalnum()]
+      ee = re.split('\w+', expr)
+      tokens = []
+      while len(temp_vars) > 0 and len(ee) > 0:
+          if len(ee[0]) < 2:
+              tokens.append(ee[0])
+          else:
+              for c in ee[0]:
+                  tokens.append(c)
+          del ee[0]
+          tokens.append(temp_vars[0])
+          del temp_vars[0]
+  
+      while len(ee) > 0:
+          if len(ee[0]) < 2:
+              tokens.append(ee[0])
+          else:
+              for c in ee[0]:
+                  tokens.append(c)
+          del ee[0]
+      variables = [x for x in re.split('\W+', expr) if x.isalnum()]
+      return variables, tokens
+  
+    def evaluate(self, expr: str):
+      """
+      evaluates the given expression with variable values stored in the calculator.
+      :param expression: str type; the mathematical expression
+      """
+      parse_tree = self._build_parse_tree(expr)
+      return self._evaluate(parse_tree.r)
+
+
+
--- tester.py
+++ tester.py
@@ -1,26 +1,19 @@
+import BookStore
 from SLLQueue import SLLQueue
 from SLLStack import SLLStack
 from DLList import DLList
 from DLLDeque import DLLDeque
 from MaxQueue import MaxQueue
+from BinaryTree import BinaryTree
+from BinarySearchTree import BinarySearchTree
 import random
 
 def test():
     """write your own tester in this function"""
-    print("Testing MaxQueue...")
-    mq = MaxQueue()
-    import random
+    print("Running your tester...")
+    bookstore = BookStore.BookStore()
+    bookstore.load_catalog("books_1.txt", "1")  # Use a small test file
+    bookstore.search_by_prefix("a", 1000)
 
-    for i in range(6):
-        value = random.randint(1, 20)
-        mq.add(value)
-        print("Added value:", value)
-        print(f"Max Queue: {mq}")
-        print(f"Max value: {mq.max()}\n")
+    
 
-    while mq.size() > 0:
-        r = mq.remove()
-        print(f"Removed:", r)
-        print(f"MaxQueue:", mq)
-        if mq.size() > 0:
-            print(f"Max value: {mq.max()}\n")
